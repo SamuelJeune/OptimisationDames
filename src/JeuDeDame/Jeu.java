dc3afaa6e06b65e2cc072c75e57d087ecfdfdc66
sam <sam-j@hotmail.fr>
2017-03-15 17:41:00 +0100

    Recuit simulé affichage du resultat

---
+++ src/JeuDeDame/Jeu.java
@@ -0,0 +1,22 @@
+package JeuDeDame;
+
+import AlgoRecuit.RecuitSimule;
+
+/**
+ * Created by sam on 15/03/17.
+ */
+public class Jeu {
+
+    int n;
+    int[] positionPlateau = new int[n-1];
+
+
+    public static void main(String[] args){
+        RecuitSimule recuitSimule = new RecuitSimule(100000,3,100,0.5);
+    }
+
+
+
+
+
+}
