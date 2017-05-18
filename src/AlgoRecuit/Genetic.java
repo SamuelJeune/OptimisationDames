1d71a578f0581634aa9962f04efcb6a77ed3f984
sam <sam-j@hotmail.fr>
2017-05-18 10:07:16 +0200

    first version of a genetic algo

--- src/AlgoRecuit/Genetic.java
+++ src/AlgoRecuit/Genetic.java
@@ -7,7 +7,7 @@ import java.util.*;
  */
 public class Genetic {
 
-    private final int NBSOLUTION=2;
+    private final int NBSOLUTION=6;
     private final int NBCURRENTPOP=6;
     private List<int[]> currentPop;
     private List<int[]> choosenPop;
@@ -20,9 +20,16 @@ public class Genetic {
         this.choosenPop=new ArrayList<>();
         this.random=new Random();
 
-        init();
-        reproduction();
+        while(true) {
+            init();
+            reproduction();
+            croisement();
+            mutation();
 
+            for (int[] solution : choosenPop) {
+                System.out.println(Util.getFitness(solution, n));
+            }
+        }
     }
 
     public void init(){
@@ -35,37 +42,65 @@ public class Genetic {
         }
     }
 
+    /**
+     * Reproduction
+     * a method to reproduce some solution based on natural selection
+     * that eliminate the solution with the greatest fitness
+     * */
     public void reproduction(){
-        /*List<Integer> pondertionList = new ArrayList<>();
+        List<Integer> weightList = new ArrayList<>();
         List<Pair> fitness = new ArrayList<>();
+        /** Create a list with the fitness of the current population */
         for (int i=0; i<currentPop.size(); i++) {
-            fitness.add(new Pair(i, Util.getFitness(currentPop.get(i), n)));
-            System.out.println(fitness.get(i).getKey()+":"+fitness.get(i).getValue()+" - ");
+            fitness.add(new Pair(Util.getFitness(currentPop.get(i), n), i));
+            //System.out.println(fitness.get(i).getKey()+":"+fitness.get(i).getValue()+" - ");
         }
+        /** Sort the fitness list in a reverse order */
         fitness.sort(new Comparator<Pair>() {
             @Override
             public int compare(Pair o1, Pair o2) {
-                return o1.compareTo(o2);
+                return o2.getValue()-o1.getValue();
             }
         });
+        /** Create a list weight for each solution that depends on their fitness */
         for (int i=0; i<fitness.size(); i++) {
-            System.out.println(fitness.get(i).getKey()+":"+fitness.get(i).getValue()+" - ");
+            //System.out.println(fitness.get(i).getKey()+":"+fitness.get(i).getValue()+" - ");
+            for(int j=0; j<=i; j++){
+                weightList.add(fitness.get(i).getKey());
+            }
+        }
+       /*for (int i=0; i<weightList.size(); i++){
+            System.out.println(weightList.get(i));
         }*/
 
-
-        for(int i=0; i<choosenPop.size();i++){
-            int k = random.nextInt(currentPop.size());
-            choosenPop.add(currentPop.get(k));
+       /** Select the solution from the current population in function of their weight */
+        for(int i=0; i<NBSOLUTION;i++){
+            int k = random.nextInt(weightList.size());
+            choosenPop.add(currentPop.get(weightList.get(k)));
         }
+
+        /*for (int[] aChoosenPop : choosenPop) {
+            System.out.println(Arrays.toString(aChoosenPop));
+        }*/
     }
 
 
-    public void croisement(){
 
+    public void croisement(){
+        for(int i=0; i<choosenPop.size(); i+=2){
+            for(int j=random.nextInt(choosenPop.get(i).length); j<choosenPop.get(i).length;j++){
+                int x = choosenPop.get(i)[j];
+                choosenPop.get(i)[j]=choosenPop.get(i+1)[j];
+                choosenPop.get(i+1)[j]=x;
+            }
+        }
     }
 
 
     public void mutation(){
-        
+        int rand = random.nextInt(choosenPop.size()-1);
+        int rand2 = random.nextInt(choosenPop.get(rand).length-1);
+        int rand3 = random.nextInt(n-1);
+        choosenPop.get(rand)[rand2]=rand3;
     }
 }
