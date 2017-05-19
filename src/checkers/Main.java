737f4e1e38aa31f00fcc0693775dde777e27fbcd
thethego <theo.goria@etu.univ-lyon1.fr>
2017-05-19 19:29:21 +0200

    refactoring

--- src/checkers/Main.java
+++ src/checkers/Main.java
@@ -1,7 +1,6 @@
-package JeuDeDame;
+package checkers;
 
 import algo.tabou.Tabou;
-import algo.recuit.RecuitSimule;
 
 /**
  * Created by hagoterio on 19/05/17.
@@ -10,7 +9,7 @@ public class Main {
 
     public static void main(String[] args){
 //       RecuitSimule recuitSimule = new RecuitSimule(100,2000,10000,0.8);
-        Tabou tabou = new Tabou(500, 100);
+        Tabou tabou = new Tabou(500, 500);
     }
 
 }
