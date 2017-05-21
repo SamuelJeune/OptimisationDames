da407fafdc073ce4bc28a57c17e21e5350d95d51
thethego <thethe71@hotmail.fr>
2017-05-21 19:31:33 +0200

    add test recuit

--- src/main/MainTabou.java
+++ src/main/MainTabou.java
@@ -2,13 +2,15 @@ package main;
 
 import algo.tabou.Tabou;
 
+import java.util.Arrays;
+
 /**
  * Created by hagoterio on 19/05/17.
  */
 public class MainTabou {
 
 
-    private final static int N = 100;
+    private final static int N = 8;
     private final static int N_MAX = 500;
 
     public static void main(String[] args){
@@ -17,6 +19,7 @@ public class MainTabou {
         tabou.run();
         long endTime = System.currentTimeMillis();
         System.out.println(tabou.getSolFinal());
+        System.out.println(Arrays.toString(tabou.getSolFinal().getBoard()));
         System.out.println("nombre d'iterations = "+tabou.getNbIteration());
         System.out.println("fitness = "+tabou.getSolFinal().getFitness());
         System.out.println("temps d'execution : " + Util.getTime((endTime - startTime)));
---
+++ src/main/testRecuitParameters.java
@@ -0,0 +1,99 @@
+package main;
+
+import algo.recuit.RecuitSimule;
+
+/**
+ * Created by hagoterio on 21/05/17.
+ */
+public class testRecuitParameters {
+
+    private static int n;
+    private static int nbIteration;
+    private static int tempInit;
+    private static double mu;
+    private static int nbTest = 20;
+    private static long meanTime;
+    private static int nbFailure;
+
+    public static void main(String[] args){
+        int[] tabN = new int[]{20,50,100,200};
+        int[] tabIteration = new int[]{100,500,1000,10000};
+        int[] tabTempInit = new int[]{100,500,1000,10000};
+        double[] tabMu = new double[]{0.2,0.5,0.7,0.8,0.9};
+        int nbTest = 20;
+
+        resetValues();;
+
+        for(int l=0;l<tabMu.length;l++){
+            mu=tabMu[l];
+            System.out.println("test avec mu = "+tabMu[l]);
+            nbFailure = 0;
+            meanTime = 0;
+            execute();
+            meanTime /= nbTest;
+            System.out.println("pourcentage de réussite = "+(100-(double)nbFailure*100/nbTest));
+            System.out.println("temps d'execution moyen : " + Util.getTime(meanTime));
+        }
+
+
+        resetValues();
+
+        for(int i=0;i<tabIteration.length;i++){
+            nbIteration=tabIteration[i];
+            System.out.println("test avec nbIteration = "+nbIteration);
+            nbFailure = 0;
+            meanTime = 0;
+            execute();
+            meanTime /= nbTest;
+            System.out.println("pourcentage de réussite = "+(100-(double)nbFailure*100/nbTest));
+            System.out.println("temps d'execution moyen : " + Util.getTime(meanTime));
+        }
+
+/*        for(int i=0;i<tabN.length;i++){
+            System.out.println("test avec n = "+tabN[i]);
+            for(int j=0;j<tabIteration.length;j++){
+                System.out.println("test avec nbIterations = "+tabIteration[j]);
+                for(int k=0;k<tabTempInit.length;k++){
+                    System.out.println("test avec température initiale = "+tabTempInit[k]);
+                    for(int l=0;l<tabMu.length;l++){
+                        System.out.println("test avec mu = "+tabMu[l]);
+                        nbFailure = 0;
+                        meanTime = 0;
+                        for(int m=0;m<5;m++){
+                            long startTime = System.currentTimeMillis();
+                            RecuitSimule recuitSimule = new RecuitSimule(tabN[i],tabIteration[j],tabTempInit[k],tabMu[l]);
+                            recuitSimule.run();
+                            long endTime = System.currentTimeMillis();
+                            if(!recuitSimule.getSolFinal().isSuccess()) nbFailure++;
+                            meanTime += endTime - startTime;
+                        }
+                        meanTime /= 5;
+                        System.out.println("pourcentage de réussite = "+(100-(double)nbFailure*100/5));
+                        System.out.println("temps d'execution moyen : " + Util.getTime(meanTime));
+                    }
+                }
+            }
+        }*/
+
+    }
+
+    private static void resetValues(){
+        n=50;
+        nbIteration = 5000;
+        tempInit = 1000;
+        mu = 0.7;
+    }
+
+    private static void execute(){
+        meanTime = 0;
+        nbFailure = 0;
+        for(int k=0;k<nbTest;k++){
+            long startTime = System.currentTimeMillis();
+            RecuitSimule recuitSimule = new RecuitSimule(n,nbIteration,tempInit,mu);
+            recuitSimule.run();
+            long endTime = System.currentTimeMillis();
+            if(!recuitSimule.getSolFinal().isSuccess()) nbFailure++;
+            meanTime += endTime - startTime;
+        }
+    }
+}
